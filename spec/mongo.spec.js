e5ae31636f0449ca3a255a1cf228130d45c93d71
Caique Rodrigues <caiquepeixoto1@gmail.com>
2018-06-01 00:52:15 -0300

    Add support to pass custom connection options.

--- spec/mongo.spec.js
+++ spec/mongo.spec.js
@@ -1,4 +1,6 @@
+const { MongoError, MongoNetworkError } = require('mongodb');
 const uuid = require('uuid-random');
+
 const mongo = require('lib/mongo');
 
 describe('mongo', () => {
@@ -31,16 +33,6 @@ describe('mongo', () => {
       expect(next).toHaveBeenCalled();
     });
 
-    it('connects with useNewUrlParser', async () => {
-      const next = jest.fn();
-      const ctx = {};
-
-      await mongo()(ctx, next);
-
-      expect(ctx.mongo.s.options.useNewUrlParser).toBeTruthy();
-      expect(next).toHaveBeenCalled();
-    });
-
     it('persists an object', async () => {
       const ctx = {};
 
@@ -60,59 +52,105 @@ describe('mongo', () => {
     });
   });
 
-  describe('using user options', () => {
-    it('builds URL with host passed via userOptions', async () => {
-      const userOptions = {
+  describe('passing url options', () => {
+    it('builds with host passed via urlOptions', async () => {
+      const urlOptions = {
         host: '127.0.0.1',
       };
 
       const next = jest.fn();
       const ctx = {};
 
-      await mongo(userOptions)(ctx, next);
+      await mongo(urlOptions)(ctx, next);
 
       expect(ctx.mongo.s.url).toBe('mongodb://127.0.0.1:27017/test');
       expect(next).toHaveBeenCalled();
     });
 
-    it('builds URL with port passed via userOptions', async () => {
-      const userOptions = {
+    it('builds with port passed via urlOptions', async () => {
+      const urlOptions = {
         port: '27017',
       };
 
       const next = jest.fn();
       const ctx = {};
 
-      await mongo(userOptions)(ctx, next);
+      await mongo(urlOptions)(ctx, next);
 
       expect(ctx.mongo.s.url).toBe('mongodb://localhost:27017/test');
       expect(next).toHaveBeenCalled();
     });
 
-    it('builds URL with database name passed via userOptions', async () => {
-      const userOptions = {
+    it('builds with database name passed via urlOptions', async () => {
+      const urlOptions = {
         databaseName: 'another-database-name',
       };
 
       const next = jest.fn();
       const ctx = {};
 
-      await mongo(userOptions)(ctx, next);
+      await mongo(urlOptions)(ctx, next);
 
       expect(ctx.mongo.s.url).toBe('mongodb://localhost:27017/another-database-name');
       expect(next).toHaveBeenCalled();
     });
   });
 
+  describe('passing connection options', () => {
+    it('connects with useNewUrlParser by default', async () => {
+      const next = jest.fn();
+      const ctx = {};
+
+      await mongo()(ctx, next);
+
+      expect(ctx.mongo.s.options.useNewUrlParser).toBeTruthy();
+      expect(next).toHaveBeenCalled();
+    });
+
+    it('connects with properties from connectionOptions', async () => {
+      const connectionOptions = {
+        useNewUrlParser: false,
+      };
+
+      const next = jest.fn();
+      const ctx = {};
+
+      await mongo({}, connectionOptions)(ctx, next);
+
+      expect(ctx.mongo.s.options.useNewUrlParser).toBeFalsy();
+      expect(next).toHaveBeenCalled();
+    });
+
+    it('try to authenticate with credentials from connectionOptions', async () => {
+      const connectionOptions = {
+        auth: {
+          user: 'wrong-user',
+          password: 'wrong-password',
+        },
+      };
+
+      const next = jest.fn();
+      const ctx = {};
+
+      await mongo({}, connectionOptions)(ctx, next);
+
+      expect(ctx.mongo).toBeUndefined();
+      expect(ctx.body.success).toBeFalsy();
+      expect(ctx.body.message).toEqual('Authentication failed.');
+      expect(ctx.body.error).toEqual(new MongoError('Authentication failed.'));
+      expect(next).toHaveBeenCalledTimes(0);
+    });
+  });
+
   describe('when connection fails', () => {
-    const userOptions = {
+    const urlOptions = {
       host: 'bad-host-to-break',
     };
 
     it('does not calls next', async () => {
       const next = jest.fn();
 
-      await mongo(userOptions)({}, next);
+      await mongo(urlOptions)({}, next);
 
       expect(next).toHaveBeenCalledTimes(0);
     });
@@ -121,7 +159,7 @@ describe('mongo', () => {
       const next = jest.fn();
       const ctx = {};
 
-      await mongo(userOptions)(ctx, next);
+      await mongo(urlOptions)(ctx, next);
 
       expect(ctx.mongo).toBeUndefined();
       expect(next).toHaveBeenCalledTimes(0);
@@ -131,11 +169,12 @@ describe('mongo', () => {
       const next = jest.fn();
       const ctx = {};
 
-      await mongo(userOptions)(ctx, next);
+      await mongo(urlOptions)(ctx, next);
 
       expect(ctx.body.success).toBeFalsy();
-      expect(ctx.body.message).toEqual('Connection to MongoDB could not be estabilished.');
-      expect(ctx.body.error).toBeDefined();
+      expect(ctx.body.message).toEqual('failed to connect to server [bad-host-to-break:27017] on first connect [MongoNetworkError: getaddrinfo ENOTFOUND bad-host-to-break bad-host-to-break:27017]');
+      expect(ctx.body.error).toEqual(new MongoNetworkError('failed to connect to server [bad-host-to-break:27017] on first connect [MongoNetworkError: getaddrinfo ENOTFOUND bad-host-to-break bad-host-to-break:27017]'));
+
       expect(next).toHaveBeenCalledTimes(0);
     });
   });
