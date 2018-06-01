eaa7bdb1acaf314bdd31c5ab7992347003de32d4
Caique Rodrigues <caiquepeixoto1@gmail.com>
2018-06-01 02:23:29 -0300

    Configure Travis CI and Codecov.

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,6 @@ module.exports = {
   globalSetup: './spec/environment/setup.js',
   globalTeardown: './spec/environment/teardown.js',
   testEnvironment: './spec/environment/mongo-environment.js',
+  coverageDirectory: './coverage/',
+  collectCoverage: true,
 };
